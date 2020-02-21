398e502fae007dc2a274f78797506cc34c6df205
rgarcia87 <garcia.muela@gmail.com>
2020-02-21 20:52:07 +0100

    Tests on water

---
+++ test-14-updated-input-Sergio/04-simplex/simplify4.sh
@@ -0,0 +1,14 @@
+#/bin/bash 
+
+sed -i "s/g000000/i000000/g" $1 
+
+sed -i "/i4/d" $1 
+sed -i "/i3/d" $1 
+sed -i "/i2/d" $1 
+sed -i "/i1/d" $1 
+
+sed -i "/g4/d" $1  
+sed -i "/g3/d" $1  
+sed -i "/g2/d" $1 
+sed -i "/g1/d" $1 
+
